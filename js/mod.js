3520c98fd6a3a69dce7f11bf947fd4797f563404
AverageCrisis <lululovesminecraft4e@gmail.com>
2024-03-21 19:43:10 -0400

    Update mod.js
    nothing

--- js/mod.js
+++ js/mod.js
@@ -1,6 +1,6 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
+	name: "The Point Tree",
+	id: "pointtreeincrementalgame",
 	author: "nobody",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
